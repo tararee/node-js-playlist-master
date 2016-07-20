6e92d466126ca0eb996a675aa03e4a509673575a
Cory Gackenheimer <cory.gack@gmail.com>
2016-07-20 15:19:08 -0400

    Add bcrypt, hash pass

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,16 +1,17 @@
 var router = require('express').Router();
 var sequelize = require('../db.js');
 var User = sequelize.import('../models/user');
+var bcrypt = require('bcryptjs');
 
 router.post('/', function(req, res) {
 
 	var username = req.body.user.username;
-	var pass = req.body.user.password; // TODO: hash this password
+	var pass = req.body.user.password;
 	// User model communicates with 
 	// Postgres to create a new user
 	User.create({
 		username: username,
-		passwordhash: '' //TODO: make it hashed	
+		passwordhash: bcrypt.hashSync(pass, 10)
 	}).then(
 		function createSuccess(user) {
 			res.json({
