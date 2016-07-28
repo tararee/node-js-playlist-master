ebc39beb48175bd88a1bb4e8c27314900ee58667
Tara <tararee@gmail.com>
2016-07-28 10:16:44 -0400

    Require functions

--- Desktop/node-js-playlist-master/app.js
+++ Desktop/node-js-playlist-master/app.js
@@ -1,3 +1,11 @@
-require ('./count');
+// this is requiring something,
+// which we need to make available via export
+//we set it as a variable, and then we can call the variable
+// from the count.js:
+
+var counter = require ('./count');
 
 console.log(counter(['shaun','crystal', 'ryu']));
+
+//So at this point, if we go to the console and
+//type node app, it will return "There are 3 elements in this array"
