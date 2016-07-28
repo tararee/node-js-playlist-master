e9141b6032d17940eb7944438f980efe55091e6f
Tara <tararee@gmail.com>
2016-07-28 11:26:13 -0400

    element.on('click') event

--- Desktop/node-js-playlist-master/app.js
+++ Desktop/node-js-playlist-master/app.js
@@ -1,15 +1,8 @@
-// this is requiring something,
-// which we need to make available via export
-//we set it as a variable, and then we can call the variable
-// from the count.js:
+var events = require('events');
+//node has a built-in module called events
 
-var stuff = require ('./stuff');
-
-console.log(stuff.counter(['shaun','crystal', 'ryu']));
-console.log(stuff.adder(5,6));
-console.log(stuff.adder(stuff.pi, 5));
-//So at this point, if we go to the console and
-//type node app, it will return
-//"There are 3 elements in this array"
-//and also "The sum of the 2 numbers is 11"
-//and also "The sum of the 2 numbers is 8.142"
+element.on('click', function(){})
+//this is an event listener
+//like in jquery
+//this would create an element that fired a function
+//every time an event was emitted (click)
