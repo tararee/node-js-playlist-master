ca2b28df0b8a2436433fd6d95f266e370ff172a1
Tara <tararee@gmail.com>
2016-07-28 11:10:01 -0400

    Object Literal exports

--- Desktop/node-js-playlist-master/stuff.js
+++ Desktop/node-js-playlist-master/stuff.js
@@ -1,17 +1,18 @@
-module.exports.counter = function(arr){
+var counter = function(arr){
 	return 'There are ' + arr.length + ' elements in this array';
 };
-//Using the `` backtick function makes it so you don't
-//have to concatenate!!!! whoa! $makes it a number!
-module.exports.adder = function(a,b){
+
+var adder = function(a,b){
 	return `The sum of the 2 numbers is ${a+b}`;
 };
 
-module.exports.pi = 3.142;
+var pi = 3.142;
 
-// We can basically keep adding properties with .exports
-module.exports.counter = counter;
-module.exports.adder = adder;
-module.exports.pi = pi;
-// We can cut out the middle man and replace the 
-// variables with module.exports.whatever
+module.exports = {
+	counter: counter,
+	adder: adder,
+	pi: pi,
+};
+
+//now we've set all the functions inside an object
+//literal
