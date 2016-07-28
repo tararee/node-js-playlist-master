9c27a5316722190bf7496d8454d6493ddbbc5885
Tara <tararee@gmail.com>
2016-07-28 14:39:05 -0400

    Event emitter

--- Desktop/node-js-playlist-master/app.js
+++ Desktop/node-js-playlist-master/app.js
@@ -1,11 +1,33 @@
 var events = require('events');
-//node has a built-in module called events
+var util = require('util');
 
-//we can create custom events that do what we want
+var Person = function(name){
+	this.name = name;
+};
+//First we put the object constructor (Person)
+//that we want to inherit something, then what
+//we want it to inherit
+util.inherits(Person, events.EventEmitter);
+//anyone created using Person constructor will inherit
+//this event constructor
 
-var myEmitter = new events.EventEmitter();
-myEmitter.on('someEvent', function(mssg){
-	console.log(mssg);
+//now we will pass in some new objects
+var james = new Person ('james');
+var mary = new Person ('mary');
+var ryu = new Person ('ryu');
+//put them in an array
+var people = [james, mary, ryu];
+
+//forEach is a js function that cycles through each 
+people.forEach(function(person){
+	person.on('speak', function(mssg){
+		console.log(person.name + ' said: ' + mssg);
+	});
 });
 
-myEmitter.emit('someEvent', 'the event was emitted');
+james.emit('speak', 'hey dudes');
+ryu.emit('speak', 'I want a curry');
+
+
+//person.on speak takes the person and attaches
+// the listen event.
